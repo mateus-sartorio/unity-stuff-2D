06dff5c65b0400a6147cb667e20e29ff11aa26b4
MateuSartorio <mateussartorio39@gmail.com>
2023-08-12 22:49:02 -0300

    Added collision with melons and started to add animation for death

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int melons = 0;
+    [SerializeField] private Text countText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Melon"))
+        {
+            Destroy(collision.gameObject);
+            melons++;
+            countText.text = $"Melons: {melons}";
+        }
+    }
+}
